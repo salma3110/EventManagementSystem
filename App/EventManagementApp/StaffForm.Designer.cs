5a839d19d1a0317297ff60c1e70a741117b21aaf
Salma Salah <salma.salahh9@gmail.com>
2026-05-11 18:07:07 +0300

    staff

--- App/EventManagementApp/StaffForm.Designer.cs
+++ App/EventManagementApp/StaffForm.Designer.cs
@@ -2,38 +2,102 @@
 {
     partial class StaffForm
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
-            {
                 components.Dispose();
-            }
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "StaffForm";
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtFname = new System.Windows.Forms.TextBox();
+            this.txtLname = new System.Windows.Forms.TextBox();
+            this.btnSaveStaff = new System.Windows.Forms.Button();
+            this.btnClearStaff = new System.Windows.Forms.Button();
+            this.dgvStaff = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStaff)).BeginInit();
+            this.SuspendLayout();
+
+            // label1 - First Name
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(65, 40);
+            this.label1.Name = "label1";
+            this.label1.Text = "First Name";
+
+            // label2 - Last Name
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(65, 90);
+            this.label2.Name = "label2";
+            this.label2.Text = "Last Name";
+
+            // label3 - Existing Staff
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(65, 200);
+            this.label3.Name = "label3";
+            this.label3.Text = "Existing Staff";
+
+            // txtFname
+            this.txtFname.Location = new System.Drawing.Point(200, 40);
+            this.txtFname.Name = "txtFname";
+            this.txtFname.Size = new System.Drawing.Size(200, 22);
+
+            // txtLname
+            this.txtLname.Location = new System.Drawing.Point(200, 90);
+            this.txtLname.Name = "txtLname";
+            this.txtLname.Size = new System.Drawing.Size(200, 22);
+
+            // btnSaveStaff
+            this.btnSaveStaff.Location = new System.Drawing.Point(65, 140);
+            this.btnSaveStaff.Name = "btnSaveStaff";
+            this.btnSaveStaff.Size = new System.Drawing.Size(120, 30);
+            this.btnSaveStaff.Text = "Save Staff";
+            this.btnSaveStaff.Click += new System.EventHandler(this.btnSaveStaff_Click);
+
+            // btnClearStaff
+            this.btnClearStaff.Location = new System.Drawing.Point(210, 140);
+            this.btnClearStaff.Name = "btnClearStaff";
+            this.btnClearStaff.Size = new System.Drawing.Size(100, 30);
+            this.btnClearStaff.Text = "Clear";
+            this.btnClearStaff.Click += new System.EventHandler(this.btnClearStaff_Click);
+
+            // dgvStaff
+            this.dgvStaff.Location = new System.Drawing.Point(65, 225);
+            this.dgvStaff.Name = "dgvStaff";
+            this.dgvStaff.Size = new System.Drawing.Size(500, 150);
+            this.dgvStaff.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvStaff.RowTemplate.Height = 24;
+
+            // StaffForm
+            this.ClientSize = new System.Drawing.Size(620, 420);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtFname);
+            this.Controls.Add(this.txtLname);
+            this.Controls.Add(this.btnSaveStaff);
+            this.Controls.Add(this.btnClearStaff);
+            this.Controls.Add(this.dgvStaff);
+            this.Name = "StaffForm";
+            this.Text = "Manage Staff";
+            this.Load += new System.EventHandler(this.StaffForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStaff)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
         }
 
-        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtFname;
+        private System.Windows.Forms.TextBox txtLname;
+        private System.Windows.Forms.Button btnSaveStaff;
+        private System.Windows.Forms.Button btnClearStaff;
+        private System.Windows.Forms.DataGridView dgvStaff;
     }
 }
--- App/EventManagementApp/StaffForm.cs
+++ App/EventManagementApp/StaffForm.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.SqlClient;
 using System.Windows.Forms;
 
 namespace EventManagementApp
@@ -15,6 +10,80 @@ namespace EventManagementApp
         public StaffForm()
         {
             InitializeComponent();
+        }
+
+        private void StaffForm_Load(object sender, EventArgs e)
+        {
+            LoadStaff();
+        }
+
+        private void LoadStaff()
+        {
+            try
+            {
+                using (SqlConnection con = DBConnection.GetConnection())
+                {
+                    con.Open();
+                    string query = @"SELECT 
+                                        staff_id AS ID,
+                                        fname    AS FirstName,
+                                        lname    AS LastName
+                                     FROM Staff
+                                     ORDER BY fname";
+
+                    SqlDataAdapter da = new SqlDataAdapter(query, con);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+                    dgvStaff.DataSource = dt;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error loading staff: " + ex.Message);
+            }
+        }
+
+        private void btnSaveStaff_Click(object sender, EventArgs e)
+        {
+            if (txtFname.Text == "" || txtLname.Text == "")
+            {
+                MessageBox.Show("Please enter both First Name and Last Name.");
+                return;
+            }
+
+            try
+            {
+                using (SqlConnection con = DBConnection.GetConnection())
+                {
+                    con.Open();
+                    string query = @"INSERT INTO Staff (fname, lname)
+                                     VALUES (@fname, @lname)";
+
+                    SqlCommand cmd = new SqlCommand(query, con);
+                    cmd.Parameters.AddWithValue("@fname", txtFname.Text);
+                    cmd.Parameters.AddWithValue("@lname", txtLname.Text);
+
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Staff saved successfully! ✅");
+                    LoadStaff();
+                    ClearFields();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void btnClearStaff_Click(object sender, EventArgs e)
+        {
+            ClearFields();
+        }
+
+        private void ClearFields()
+        {
+            txtFname.Text = "";
+            txtLname.Text = "";
         }
     }
 }
