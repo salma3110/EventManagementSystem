183fd00f7a5960f91aa8f2df9c0dcb7edb9b9bfb
salma <salmaseif76004@gmail.com>
2026-05-12 06:55:10 +0300

    Adding View Reports

--- App/EventManagementApp/ReportsForm.Designer.cs
+++ App/EventManagementApp/ReportsForm.Designer.cs
@@ -2,38 +2,90 @@
 {
     partial class ReportsForm
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
-            if (disposing && (components != null))
-            {
-                components.Dispose();
-            }
+            if (disposing && (components != null)) components.Dispose();
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblSeats = new System.Windows.Forms.Label();
+            this.lblSchedule = new System.Windows.Forms.Label();
+            this.dgvSeats = new System.Windows.Forms.DataGridView();
+            this.dgvSchedule = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSeats)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSchedule)).BeginInit();
+            this.SuspendLayout();
+
+            // Title
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F,
+                System.Drawing.FontStyle.Bold);
+            this.lblTitle.Location = new System.Drawing.Point(50, 15);
+            this.lblTitle.Text = "Tracking Seat Availability and Gathering Schedules";
+
+            // Seat availability label
+            this.lblSeats.AutoSize = true;
+            this.lblSeats.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F,
+                System.Drawing.FontStyle.Bold);
+            this.lblSeats.Location = new System.Drawing.Point(50, 55);
+            this.lblSeats.Text = "Seat Availability";
+
+            // Seat availability grid
+            this.dgvSeats.Location = new System.Drawing.Point(50, 78);
+            this.dgvSeats.Size = new System.Drawing.Size(880, 200);
+            this.dgvSeats.ReadOnly = true;
+            this.dgvSeats.SelectionMode =
+                System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvSeats.ColumnHeadersHeightSizeMode =
+                System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvSeats.AutoSizeColumnsMode =
+                System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+
+            // Schedule label
+            this.lblSchedule.AutoSize = true;
+            this.lblSchedule.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F,
+                System.Drawing.FontStyle.Bold);
+            this.lblSchedule.Location = new System.Drawing.Point(50, 300);
+            this.lblSchedule.Text = "Gathering Schedule";
+
+            // Schedule grid
+            this.dgvSchedule.Location = new System.Drawing.Point(50, 323);
+            this.dgvSchedule.Size = new System.Drawing.Size(880, 200);
+            this.dgvSchedule.ReadOnly = true;
+            this.dgvSchedule.SelectionMode =
+                System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvSchedule.ColumnHeadersHeightSizeMode =
+                System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvSchedule.AutoSizeColumnsMode =
+                System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+
+            // Form
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "ReportsForm";
+            this.ClientSize = new System.Drawing.Size(980, 560);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.lblSeats);
+            this.Controls.Add(this.dgvSeats);
+            this.Controls.Add(this.lblSchedule);
+            this.Controls.Add(this.dgvSchedule);
+            this.Name = "ReportsForm";
+            this.Text = "View Reports";
+            this.Load += new System.EventHandler(this.ReportsForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSeats)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSchedule)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
         }
 
-        #endregion
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblSeats;
+        private System.Windows.Forms.Label lblSchedule;
+        private System.Windows.Forms.DataGridView dgvSeats;
+        private System.Windows.Forms.DataGridView dgvSchedule;
     }
 }
--- App/EventManagementApp/ReportsForm.cs
+++ App/EventManagementApp/ReportsForm.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.SqlClient;
 using System.Windows.Forms;
 
 namespace EventManagementApp
@@ -15,6 +10,56 @@ namespace EventManagementApp
         public ReportsForm()
         {
             InitializeComponent();
+        }
+
+        private void ReportsForm_Load(object sender, EventArgs e)
+        {
+            LoadSeatAvailability();
+            LoadGatheringSchedule();
+        }
+
+        private void RunQuery(DataGridView dgv, string query)
+        {
+            SqlConnection con = DBConnection.GetConnection();
+            DataTable dt = new DataTable();
+            new SqlDataAdapter(query, con).Fill(dt);
+            dgv.DataSource = dt;
+        }
+
+        // Seat availability per class per gathering
+        private void LoadSeatAvailability()
+        {
+            string query = @"
+                SELECT 
+                    g.title                                 AS Gathering,
+                    ec.class_name                           AS Class,
+                    ec.price                                AS Price,
+                    ec.seats_allocation                     AS Total_Seats,
+                    COUNT(ep.pass_id)                       AS Sold,
+                    ec.seats_allocation - COUNT(ep.pass_id) AS Available
+                FROM Gathering g
+                JOIN EntryClass ec ON g.gathering_id = ec.gathering_id
+                LEFT JOIN EntryPass ep ON ec.class_id = ep.class_id
+                GROUP BY g.title, g.date, ec.class_name, ec.price, ec.seats_allocation
+                ORDER BY g.date, g.title";
+            RunQuery(dgvSeats, query);
+        }
+
+        // Gathering schedule with venue
+        private void LoadGatheringSchedule()
+        {
+            string query = @"
+                SELECT 
+                    g.title     AS Gathering,
+                    g.date      AS Date,
+                    g.category  AS Category,
+                    v.name      AS Venue,
+                    v.city      AS City,
+                    v.capacity  AS Venue_Capacity
+                FROM Gathering g
+                JOIN Venue v ON g.venue_id = v.venue_id
+                ORDER BY g.date";
+            RunQuery(dgvSchedule, query);
         }
     }
 }
